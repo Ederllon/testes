06fda5e181bb242ae0ed8929c49491e00681dcd8
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-04 10:34:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function run(){
-var options = {method: 'GET', headers: {accept: 'application/json'}};
-fetch('https://api.themoviedb.org/3/authentication', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
 
-
-}
