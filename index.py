6e9e6cdbcfc100b8c7c70e958b59901b767095af
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-07 23:22:34 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,34 +1,45 @@
-
 from random import randint
 # armas
-nenhuma = {'name':'nenhuma', 'atack' : 0}
-faca = {'name':'faca', 'atack' : 0}
-bastão = {'name':'bastão', 'atack' : 0}
-espada = {'name':'espada', 'atack' : 0}
+nenhuma = {'nome':'nenhuma', 'dano' : 0}
+faca = {'nome':'faca', 'dano' : 3}
+bastão = {'nome':'bastão', 'dano' : 6}
+espada = {'nome':'espada', 'dano' : 7}
 
 
-magias = ['fire', 'wind', 'earth', 'eletric', 'water']
+magias = ['nenhuma', 'fire', 'wind', 'earth', 'eletric', 'water']
 nmagias = magias.__len__() - 1
 armas = [nenhuma, faca, bastão, espada]
 narmas = armas.__len__() - 1
 
-hp = int()
-atk = int()
-df = int()
-spd = int()
-man = int()
-mag = int()
-magperc = int()
-arm = int()
-maestria = int()
 
 
 
 def ficha():
-    print(magias[nmagias])
-    print(armas[2])
-
-
-
-       # print("dado {} : {}" .format((count+1),randint(1,pointDice)))
-ficha()
+    hp = int(randint(1,100))
+    atk = int(randint(1,100))
+    df = int(randint(1,100))
+    spd = int(randint(1,100))
+    man = int(randint(1,100))
+    mag = int(randint(0,nmagias))
+    magperc = int(randint(1,100))
+    arm = int(randint(0,narmas))
+    maestria = int(randint(1,100))
+    if mag == 0:
+        magper = 0
+    if arm == 0:
+        maestria = 0   
+    print(60*'-')
+    print('vida:',hp)
+    print('Força:',atk)
+    print('Defesa:',df)
+    print('Velocidade:',spd)
+    print('Mana:',man)
+    print(60*'-')
+    print('Magia:',magias[mag])
+    print('Maestria da Magia: {}%'.format(magperc))
+    print(60*'-')
+    print('arma:',armas[arm])
+    print('Maestria da Arma: {}%'.format(maestria))
+    print(60*'-')
+ficha()        
+            
