d377ddfa6f9354551d6b68557a477b01b9de5490
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-07 22:17:48 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,21 @@
 
+
+from random import randint
+magias = ['fire', 'wind', 'earth', 'eletric', 'water']
+nmagias = magias.__len__() - 1
+
+
+atk = int()
+df = int()
+spd = int()
+man = int()
+
+
+
+def ficha():
+    print(magias[nmagias])
+
+
+
+       # print("dado {} : {}" .format((count+1),randint(1,pointDice)))
+ficha()
