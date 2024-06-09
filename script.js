1ac3cafe035c7198736c7716eb4daeaf629ced92
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-08 23:55:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
 
-const jokeElement = document.getElementById('joke');
-const getJokeBtn = document.getElementById('getJokeBtn');
-
-getJokeBtn.addEventListener('click', getJoke);
-
-function getJoke() {
-    fetch('https://api.chucknorris.io/jokes/random')
-    .then(response => response.json())
-    .then(data => {
-        jokeElement.textContent = data.value;
-    })
-    .catch(error => {
-        console.error('Error fetching joke:', error); 
-    });
-}
