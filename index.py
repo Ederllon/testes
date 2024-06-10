317403fa7b3b9767d1a54faf947ea6a5aab9d8cf
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-10 10:41:04 -0300

    Update index.py

--- index.py
+++ index.py
@@ -10,7 +10,6 @@ nenhuma = {'nome':'nenhuma', 'dano' : 0, 'defesa': 0, 'critico': 0, }
 faca = {'nome':'faca', 'dano' : 2, 'defesa': 1, 'critico': 6, }
 bastão = {'nome':'bastão', 'dano' : 3, 'defesa': 5, 'critico': 0, }
 espada = {'nome':'espada', 'dano' : 7, 'defesa': 6, 'critico': 10, }
-arco = {'nome':'arco', 'dano' : 3, 'defesa': 1, 'critico': 10, }
 machado = {'nome':'machado', 'dano' : 10, 'defesa': 4, 'critico': 9, }
 martelo = {'nome':'martelo', 'dano' : 7, 'defesa': 4, 'critico': 1, }
 lança = {'nome':'lança', 'dano' : 6, 'defesa': 5, 'critico': 7,}
@@ -45,15 +44,23 @@ def ficha(x):
     man = int(randint(1,100))
     magperc = int(randint(1,100))
     maestria = int(randint(1,100))
+    luck = int(randint(1,100))
+    prec = int(randint(1,100))
 
 # inicio dados com adicionaveis     
 
-    anel = {'nome':'anel de poder', 'dano' : magperc, 'defesa': man, 'critico': maestria, }
-
+    anel = {'nome':'anel do poder', 'dano' : magperc, 'defesa': man, 'critico': maestria, }
+    arco = {'nome':'arco', 'dano' : 3, 'defesa': 1, 'critico': prec, }
+    cajado = {'nome':'cajado', 'dano' : int((magperc + maestria)/2), 'defesa': man, 'critico': maestria, }
+    anel_de_energia = {'nome':'anel de energia', 'dano' : man, 'defesa': man, 'critico': man, }
+    sai = {'nome':'sai', 'dano' : 3, 'defesa': 2, 'critico': spd, }
+    soco = {'nome':'soco inglês', 'dano' : atk , 'defesa': df-30, 'critico': -1 }
+    luva = {'nom':'luvas', 'dano' : 1, 'defesa': 1, 'critico': atk-50,}
+    pedra = {'nome':'pedra', 'dano' : atk-prec, 'defesa': 0, 'critico': prec, }
 # fim dados com adicionaveis
     
     # inicio dados insert 
-    armas = [nenhuma, faca, bastão, espada, arco, machado, martelo, lança, escudo, bengala, manopla, chicote, canivete, machete, pá, foice, marreta , anel]
+    armas = [nenhuma, faca, bastão, sai,pedra, luva, cajado, espada, arco, machado, martelo, soco, lança, escudo, bengala, manopla, chicote, canivete, machete, pá, foice, marreta , anel, anel_de_energia]
     narmas = armas.__len__() - 1
     # fim dados insert
     mag = int(randint(0,nmagias))
@@ -65,12 +72,14 @@ def ficha(x):
         maestria = 0   
       
 
-    print('ficha: #{} | nivel de poder: {}'.format(x, 0       ))
+    print('Ficha: #{} | Média De Poder: {}'.format(x,  int((hp+atk+df+spd+car+man+maestria+magperc+luck+prec)/10)))
     print('Vida:',hp)
     print('Força:',atk)
     print('Defesa:',df)
     print('Velocidade:',spd)
     print('Carisma:', car)
+    print('Precisão: ',prec )
+    print('Sorte: ', luck)
     print('Mana:',man)
     print('Magia:',magias[mag])
     print('Maestria da Magia: {}%'.format(magperc))
