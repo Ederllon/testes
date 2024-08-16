ee04e3dbba44eda4cbdf77f44cb09ec63bebbf85
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-08-16 19:09:28 -0300

    Update python.py

--- python.py
+++ python.py
@@ -1,2 +1,15 @@
-
+array_ = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+req_ = int(input("Escolha um número: "))
+if req_ >= 27:
+    print("Tente novamente com número abaixo de 27")
+else:
+    def func():
+        new_list = []
+        c = 0
+        while c < req_:
+            new_list.append(array_[c])
+            c = c +1
+        print(new_list)
+    func()    
+    
 
